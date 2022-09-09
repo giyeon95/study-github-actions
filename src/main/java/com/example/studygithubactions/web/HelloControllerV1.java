c0ca45ecd06d0557d512396a464a95e54f1f0d4c
giyeon95 <giyeon15@gmail.com>
2022-09-09 16:49:22 +0900

    init

---
+++ src/main/java/com/example/studygithubactions/web/HelloControllerV1.java
@@ -0,0 +1,17 @@
+package com.example.studygithubactions.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/hello")
+@RequiredArgsConstructor
+public class HelloControllerV1 {
+
+    @GetMapping
+    public String getHello() {
+        return "hello world!";
+    }
+}
